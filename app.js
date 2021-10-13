faa1bf2bd8f1237444f98fdbd46ede5e45d57ce3
Jordan Arata <jarataraj@gmail.com>
2021-10-13 16:38:43 -0400

    connection fix 3

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 document.querySelector('#connect').addEventListener('click', event => {
     esp32.connect()
         .then(() => {
-            console.log(ESP32.device);
+            console.log(Esp32.device);
         })
         .catch(error => {
             console.error('Argh!', error);
         });
+});
+
+document.querySelector('#led').addEventListener('click', event => {
+    esp32.changeLED()
 });
--- esp32.js
+++ esp32.js
@@ -2,6 +2,7 @@
     'use strict';
 
     const SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+    const LED_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
     class Esp32 {
         constructor() {
@@ -16,7 +17,14 @@
             return navigator.bluetooth.requestDevice(options)
                 .then(device => {
                     this.device = device;
+                    return device.gatt.connect();
                 });
+        }
+
+        changeLed() {
+            return this.device.gatt.getPrimaryService(SERVICE_UUID)
+                .then(service => service.getCharacteristic(LED_UUID))
+                .then(characteristic => characteristic.writeValue("test"))
         }
     }
 
